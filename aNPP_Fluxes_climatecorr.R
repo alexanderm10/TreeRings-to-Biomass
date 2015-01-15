5f88aad4773481c868adb1929facb681ca7340d5
Ross Alexander <alexanderm10@email.arizona.edu>
2015-01-15 16:02:08 -0700

    1/15 Commit
    Gap filling code: fixed the diameter reconstruction issue from 1/14
    
    Other files: added Flurin’s data and worked through the scripts the he
    sent over.
    
    Ameriflux maps:  Have a brief mapping script for the study sites.
    Christy said we could make this spiffy.

---
+++ aNPP_Fluxes_climatecorr.R
@@ -0,0 +1,194 @@
+library(ncdf)
+# NOTE: Run site level detrending script and load objects into the workspace
+
+#load any additional chronologies (In this case Flurin's Niwot Chronologies)
+niwot.b.chrono <- ts(read.table("plotB_detrended_chronos.txt", header=T), end = 2012, frequency = 1)
+niwot.c.chrono <- ts(read.table("plotC_detrended_chronos.txt", header=T), end = 2012, frequency = 1)
+
+
+#mmf chronologies consolodated
+
+mmf.chrono <- ts(read.csv("chron_all_mmfspp.csv"), end=2014, frequency =1)
+
+# load Flux data (for now both Niwot and MMF are monthly)
+#The niwot flux record spans 1998-2013
+niwot.nep.m <- read.table("monthly_nep_niwot.txt", header=T)
+
+#The Morgan Monroe flux record spans 1999-2005
+mmf.nep.m <- read.table("monthly_nep_morgan.txt", header=T)
+
+# this code comes from Flurin's climate_response.r file
+
+###load climate data
+months <- c("Jan", "Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec")
+clim.niwot <- vector(mode = "list", length = 4); names(clim.niwot) <- c("temp", "precip", "radiation","airhumidity")
+clim.morgan <- vector(mode = "list", length = 4); names(clim.morgan) <- c("temp", "precip", "radiation","airhumidity")
+
+for(i in 1:length(clim.niwot)){
+  clim.niwot[[i]] <- ts(matrix(data = NA, ncol = 12, nrow = 110), start = 1901, frequency = 1); colnames(clim.niwot[[i]]) <- months
+  clim.morgan[[i]] <- ts(matrix(data = NA, ncol = 12, nrow = 110), start = 1901, frequency = 1); colnames(clim.morgan[[i]]) <- months
+}
+
+nNiwot <- 100; ewNiwot <- 149; nMorgan <- 102; ewMorgan <- 188  #grid cells
+
+#NOTE: need to get the CRU-nCEP data from Flurin so that I can draw off of the source code.
+#Note: This will be useful for later
+
+
+for(i in 1:110){
+  
+  
+  #setwd("C:/Users/babst/Desktop/data/data_climate/cruncep_data/tair")
+  setwd("/Users/mrossalexander/desktop/CRUNCEP_monthly/tair")
+  clim.niwot[[1]][i,] <- get.var.ncdf(open.ncdf(list.files()[i]), start = c(ewNiwot,nNiwot,1), count = c(1,1,12))-272.15   #transform to Celsius
+  clim.morgan[[1]][i,] <- get.var.ncdf(open.ncdf(list.files()[i]), start = c(ewMorgan,nMorgan,1), count = c(1,1,12))-272.15
+  
+  
+  #setwd("C:/Users/babst/Desktop/data/data_climate/cruncep_data/rain")
+  setwd("/Users/mrossalexander/desktop/CRUNCEP_monthly/rain")
+  clim.niwot[[2]][i,] <- get.var.ncdf(open.ncdf(list.files()[i]), start = c(ewNiwot,nNiwot,1), count = c(1,1,12))
+  clim.morgan[[2]][i,] <- get.var.ncdf(open.ncdf(list.files()[i]), start = c(ewMorgan,nMorgan,1), count = c(1,1,12))
+  
+  #setwd("C:/Users/babst/Desktop/data/data_climate/cruncep_data/swdown_total")
+  setwd("/Users/mrossalexander/desktop/CRUNCEP_monthly/swdown_total")
+  clim.niwot[[3]][i,] <- get.var.ncdf(open.ncdf(list.files()[i]), start = c(ewNiwot,nNiwot,1), count = c(1,1,12))
+  clim.morgan[[3]][i,] <- get.var.ncdf(open.ncdf(list.files()[i]), start = c(ewMorgan,nMorgan,1), count = c(1,1,12))
+  
+  #setwd("C:/Users/babst/Desktop/data/data_climate/cruncep_data/qair")
+  setwd("/Users/mrossalexander/desktop/CRUNCEP_monthly/qair")
+  clim.niwot[[4]][i,] <- get.var.ncdf(open.ncdf(list.files()[i]), start = c(ewNiwot,nNiwot,1), count = c(1,1,12))
+  clim.morgan[[4]][i,] <- get.var.ncdf(open.ncdf(list.files()[i]), start = c(ewMorgan,nMorgan,1), count = c(1,1,12))
+  
+}
+
+
+### correlation per species in tree rings
+
+months <- c("pJan", "pFeb","pMar","pApr","pMay","pJun","pJul","pAug","pSep","pOct","pNov","pDec","Jan", "Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec","pYear","pJJA","pAMJJAS","MAM","MJ","AMJJAS","MJJ","JJA","JJ","JA")
+
+first <- c(1901,1901,1956,1901,1910,1920,1930,1940,1950,1960,1970,1980)
+last <- c(2010,1955,2010,1930,1940,1950,1960,1970,1980,1990,2000,2010)
+
+clim.cor.niwot.tr <- vector(mode = "list", length = length(first))
+clim.cor.morgan.tr <- vector(mode = "list", length = length(first))
+names(clim.cor.niwot.tr) <- c("1901-2010","1901-1955","1956-2010","1901-1930","1910-1940", "1920-1950", "1930-1960", "1940-1970", "1950-1980", "1960-1990", "1970-2000", "1980-2010")
+names(clim.cor.morgan.tr) <- names(clim.cor.niwot.tr)
+
+for(x in 1:length(clim.cor.niwot.tr)){
+  
+  periodN <- vector(mode = "list", length = 4); names(periodN) <- c("temp", "precip", "radiation","airhumidity")
+  periodM<- vector(mode = "list", length = 4); names(periodM) <- c("temp", "precip", "radiation","airhumidity")
+  
+  
+  for(i in 1:length(periodN)){
+    periodN[[i]] <- matrix(data = NA, ncol = 34, nrow = ncol(niwot.b.chrono)); colnames(periodN[[i]]) <- months; rownames(periodN[[i]]) <- colnames(niwot.b.chrono)
+    periodM[[i]] <- matrix(data = NA, ncol = 34, nrow = (ncol(mmf.chrono)-1)); colnames(periodM[[i]]) <- months; rownames(periodM[[i]]) <- colnames(mmf.chrono[,2:ncol(mmf.chrono)])
+  }
+  
+  for(i in 1:length(periodN)){
+    for(a in 1:ncol(niwot.b.chrono)){
+      periodN[[i]][a,] <- as.numeric(fancymonthlycorrels(niwot.b.chrono[,a],clim.niwot[[i]],first[x],last[x]))
+    }
+  }
+  
+  for(i in 1:length(periodM)){
+    for(a in 2:ncol(mmf.chrono)){
+      periodM[[i]][(a-1),] <- as.numeric(fancymonthlycorrels(mmf.chrono[,a],clim.morgan[[i]],first[x],last[x]))
+    }
+  }
+  
+  
+  clim.cor.niwot.tr[[x]] <- periodN
+  clim.cor.morgan.tr[[x]] <- periodM
+  
+}
+
+# Correlate climate with fluxes
+
+
+months <- c("pJan", "pFeb","pMar","pApr","pMay","pJun","pJul","pAug","pSep","pOct","pNov","pDec","Jan", "Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec","pYear","pJJA","pAMJJAS","MAM","MJ","AMJJAS","MJJ","JJA","JJ","JA")
+
+first <- c(1901,1901,1956,1901,1910,1920,1930,1940,1950,1960,1970,1980)
+last <- c(2010,1955,2010,1930,1940,1950,1960,1970,1980,1990,2000,2010)
+
+clim.cor.niwot.flux <- vector(mode = "list", length = length(first))
+clim.cor.morgan.flux <- vector(mode = "list", length = length(first))
+names(clim.cor.niwot.flux) <- c("1901-2010","1901-1955","1956-2010","1901-1930","1910-1940", "1920-1950", "1930-1960", "1940-1970", "1950-1980", "1960-1990", "1970-2000", "1980-2010")
+names(clim.cor.morgan.flux) <- names(clim.cor.niwot.flux)
+
+for(x in 1:length(clim.cor.morgan.flux)){
+  
+  periodN <- vector(mode = "list", length = 4); names(periodN) <- c("temp", "precip", "radiation","airhumidity")
+  periodM<- vector(mode = "list", length = 4); names(periodM) <- c("temp", "precip", "radiation","airhumidity")
+  
+  
+  for(i in 1:length(periodN)){
+    periodN[[i]] <- matrix(data = NA, ncol = 34, nrow = ncol(niwot.nep.m)); colnames(periodN[[i]]) <- months; rownames(periodN[[i]]) <- colnames(niwot.nep.m)
+    periodM[[i]] <- matrix(data = NA, ncol = 34, nrow = (ncol(mmf.nep.m)-1)); colnames(periodM[[i]]) <- months; rownames(periodM[[i]]) <- colnames(mmf.nep.m[,2:ncol(mmf.nep.m)])
+  }
+  
+  for(i in 1:length(periodN)){
+    for(a in 1:ncol(niwot.nep.m)){
+      periodN[[i]][a,] <- as.numeric(fancymonthlycorrels(niwot.nep.m[,a],clim.niwot[[i]],first[x],last[x]))
+    }
+  }
+  
+  for(i in 1:length(periodM)){
+    for(a in 2:ncol(mmf.nep.m)){
+      periodM[[i]][(a-1),] <- as.numeric(fancymonthlycorrels(mmf.nep.m[,a],clim.morgan[[i]],first[x],last[x]))
+    }
+  }
+  
+  
+  clim.cor.morgan.flux[[x]] <- periodN
+  clim.cor.morgan.flux[[x]] <- periodM
+  
+}
+
+
+#plotting graphs for climate response
+
+#Niwot
+
+par(mfrow = c(2,2))
+barplot(clim.cor.niwot.tr[[3]][[1]][,1:24], beside = T, ylim = c(-0.6,0.6), main = "temperature")  #temp
+abline(h = c(-0.235,0.235), lty = "dashed")
+barplot(clim.cor.niwot.tr[[3]][[2]][,1:24], beside = T, ylim = c(-0.6,0.6), main = "precipitation")  #precip
+abline(h = c(-0.235,0.235), lty = "dashed")
+barplot(clim.cor.niwot.tr[[3]][[3]][,1:24], beside = T, ylim = c(-0.6,0.6), main = "SW radiation")  #rad
+abline(h = c(-0.235,0.235), lty = "dashed")
+barplot(clim.cor.niwot.tr[[3]][[1]][,1:24], beside = T, ylim = c(-0.6,0.6), main = "abs. humidity")  #hum
+abline(h = c(-0.235,0.235), lty = "dashed")
+
+par(mfrow = c(2,2))
+barplot(clim.cor.niwot.tr[[2]][[1]][,25:34], beside = T, ylim = c(-0.6,0.6), main = "temperature")  #temp
+abline(h = c(-0.235,0.235), lty = "dashed")
+barplot(clim.cor.niwot.tr[[2]][[2]][,25:34], beside = T, ylim = c(-0.6,0.6), main = "precipitation")  #precip
+abline(h = c(-0.235,0.235), lty = "dashed")
+barplot(clim.cor.niwot.tr[[2]][[3]][,25:34], beside = T, ylim = c(-0.6,0.6), main = "SW radiation")  #rad
+abline(h = c(-0.235,0.235), lty = "dashed")
+barplot(clim.cor.niwot.tr[[2]][[1]][,25:34], beside = T, ylim = c(-0.6,0.6), main = "abs. humidity")  #hum
+abline(h = c(-0.235,0.235), lty = "dashed")
+
+
+#Morgan
+
+par(mfrow = c(2,2))
+barplot(clim.cor.morgan.tr[[3]][[1]][,1:24], beside = T, ylim = c(-0.6,0.6), main = "temperature")  #temp
+abline(h = c(-0.235,0.235), lty = "dashed") # 0.235 is the significance threshhold for 50 years of data
+barplot(clim.cor.morgan.tr[[3]][[2]][,1:24], beside = T, ylim = c(-0.6,0.6), main = "precipitation")  #precip
+abline(h = c(-0.235,0.235), lty = "dashed")
+barplot(clim.cor.morgan.tr[[3]][[3]][,1:24], beside = T, ylim = c(-0.6,0.6), main = "SW radiation")  #rad
+abline(h = c(-0.235,0.235), lty = "dashed")
+barplot(clim.cor.morgan.tr[[3]][[1]][,1:24], beside = T, ylim = c(-0.6,0.6), main = "abs. humidity")  #hum
+abline(h = c(-0.235,0.235), lty = "dashed")
+
+par(mfrow = c(2,2))
+barplot(clim.cor.morgan.tr[[2]][[1]][,25:34], beside = T, ylim = c(-0.6,0.6), main = "temperature")  #temp
+abline(h = c(-0.235,0.235), lty = "dashed")
+barplot(clim.cor.morgan.tr[[2]][[2]][,25:34], beside = T, ylim = c(-0.6,0.6), main = "precipitation")  #precip
+abline(h = c(-0.235,0.235), lty = "dashed")
+barplot(clim.cor.morgan.tr[[2]][[3]][,25:34], beside = T, ylim = c(-0.6,0.6), main = "SW radiation")  #rad
+abline(h = c(-0.235,0.235), lty = "dashed")
+barplot(clim.cor.morgan.tr[[2]][[1]][,25:34], beside = T, ylim = c(-0.6,0.6), main = "abs. humidity")  #hum
+abline(h = c(-0.235,0.235), lty = "dashed")
---
+++ ameriflux_maps_NACP15_poster.R
@@ -0,0 +1,43 @@
+# Mapping Spatial Points, Basic
+
+library(maps)
+library(maptools)
+
+###############
+# Read in data table
+dave.sites <- read.csv("sites_dave.csv", header=T) 
+
+evergreen <- dave.sites[dave.sites$type == "Evergreen Needle-leaf Forest",]
+deciduous <- dave.sites[dave.sites$type == "Deciduous Broad-leaf Forest",]
+mixed <- dave.sites[dave.sites$type == "Mixed Forest",]
+
+mmf.map <- dave.sites[dave.sites$site.name == "Morgan Monroe State Forest",]
+niwot.map <- dave.sites[dave.sites$site.name == "Niwot Ridge",]
+
+# making a spatial points data frame, guessing on coordinate system
+dave.sites <- SpatialPointsDataFrame(coords=dave.sites[,c("long", "lat")], dave.sites, proj4string=CRS("+proj=longlat +datum=WGS84"))
+evergreen <- SpatialPointsDataFrame(coords=evergreen[,c("long", "lat")], evergreen, proj4string=CRS("+proj=longlat +datum=WGS84"))
+deciduous <- SpatialPointsDataFrame(coords=deciduous[,c("long", "lat")], deciduous, proj4string=CRS("+proj=longlat +datum=WGS84"))
+mixed <- SpatialPointsDataFrame(coords=mixed[,c("long", "lat")], mixed, proj4string=CRS("+proj=longlat +datum=WGS84"))
+mmf.map <- SpatialPointsDataFrame(coords=mmf.map[,c("long", "lat")], mmf.map, proj4string=CRS("+proj=longlat +datum=WGS84"))
+niwot.map <- SpatialPointsDataFrame(coords=niwot.map[,c("long", "lat")], niwot.map, proj4string=CRS("+proj=longlat +datum=WGS84"))
+
+
+map("state", plot=T, lty="solid", col="gray50", lwd=2)
+legend("bottomleft", legend=c("Active Sites", "Summer 2014", "Sampled Sites", "Summer 2015"), pch=19, col=c("red", "blue", "green", "orange"), cex+.5)
+
+
+
+pdf("NACP15_sitemap.pdf", height=8, width=11)
+
+map("state", plot=T, lty="solid", col="gray50", lwd=2)
+#plot(dave.sites, pch=19, add=T, cex=1)
+plot(evergreen, pch=15, add=T, cex=2, col = "dark green")
+plot(deciduous, pch=19, add=T, cex=2, col = "green")
+plot(mixed, pch=18, add=T, cex=2, col = "blue")
+plot(mmf.map, pch=15, add=T, cex=3, col = "red")
+plot(niwot.map, pch=19, add=T, cex=3, col= "deeppink")
+
+legend("bottomleft", legend=c("Evergreen", "Deciduous", "Mixed", "Morgan Monroe", "Niwot Ridge"), pch=c(15,19,18,15,19), col=c("dark green", "green", "blue", "red", "deeppink"), cex=1)
+
+dev.off()
