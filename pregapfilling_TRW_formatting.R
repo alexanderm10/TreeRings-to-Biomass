5edc9db898066f20af859fc861eb3311eff5affe
crollinson <crollinson@gmail.com>
2015-01-17 14:15:16 -0500

    Exporting Ring Widths for gapfilling
    write Tree-level rings widths to a file to be used for gap filling

--- pregapfilling_TRW_formatting.R
+++ pregapfilling_TRW_formatting.R
@@ -74,19 +74,79 @@ for(j in colnames(core.rw)){ # rather than going by number, we're using names to
 		}
 	}
 }
+summary(core.rw)
 # ----------------------------------------------------------------------------
 
 
+##########################################################################
+#AGGREGATION STEP
+#taking ring widths and aggregating from the core level to the tree level
+##########################################################################
+# ----------------------------------------------------------------------------
+# aggregate to the tree level using only dated trees where possible
+trees <- unique(substr(names(core.rw), 1, 6)) # listing trees we have measurements for
+tree.rw <- data.frame(array(NA, dim=c(nrow(core.rw), length(trees)))) # a blank data frame to put everything in
+row.names(tree.rw) <- row.names(core.rw)  # labeling the rows with the years from our rwl
+names(tree.rw)<-unique(substr(names(core.rw), 1, 6)) # labeling the columns as trees
+# summary(tree.rw) # this will get really big very quickly
+dim(tree.rw) # 90 trees, 112 years of data
+for(i in unique(trees)){
+  cols <- which(substr(names(core.rw),1,6)==i) # getting the columns we're working with
+  cores <- names(core.rw)[cols] # getting the name of the cores we're working with
+  
+  if(length(cols) == 1){ # if there's only one core, we just take that regardless of wheter it's dated or not
+   	tree.rw[,which(trees==i)] <- core.rw[,cols]
+  	} else { 
+  	# if there's more than 1 core, we need to figure out which if any were dated
+  	use <- vector(length=length(cols))
+	for(x in 1:length(cols)){
+		ifelse(core.data[core.data$CoreID==cores[x], "dated"]=="Y", use[x] <- "TRUE", use[x] <- "FALSE")
+	 }	
 
+	# now we know which were dated, so we can use that to figure out which cores to average
+	if(length(use[use=="TRUE"])==1) { # if only 1 core is dated, use only that core
+  	 	tree.rw[,which(trees==i)] <- core.rw[,cols[which(use=="TRUE")]]
+	} else if(length(use[use=="TRUE"])>1) { # If there's greater than one dated core, take the mean of the dated cores
+		tree.rw[,which(trees==i)] <- rowMeans(core.rw[,cols[which(use=="TRUE")]], na.rm=T)
+	} else { # If no cores are dated, take the mean of whatever we have
+		tree.rw[,which(trees==i)] <- rowMeans(core.rw[,cols], na.rm=T) 
+	}
+  }
+}
+# summary(tree.rw)
+min(tree.rw, na.rm=T); max(tree.rw, na.rm=T)
+dim(tree.rw)
+# ----------------------------------------------------------------------------
+
+# ----------------------------------------------------------------------------
+# ----------------------------------------------------------------------------
+# CRR: writing our tree RWL to a csv formatted for gap filling before we calculate BAI (bc some have missing outsides and that's bad if we're working outside-in in out calculations)
+
+# stacking the RWL so that we have a single column of ring widths & an identifying column with TreeID
+tree.stack <- stack(tree.rw)
+names(tree.stack) <- c("RW", "TreeID")
+tree.stack$Year <- as.numeric(row.names(tree.rw)) # adding in the years
+summary(tree.stack)
+
+# attaching all of our useful tree data
+summary(tree.data)
+tree.stack <- merge(tree.stack, tree.data, all.x=T, all.y=F)
+summary(tree.stack)
+dim(tree.stack)
+
+write.csv(tree.rw, "TreeRWL_stacked.csv", row.names=T)
+# ----------------------------------------------------------------------------
+# ----------------------------------------------------------------------------
+
+# ----------------------------------------------------------------------------
+# I think this next block can get deleted
+# ----------------------------------------------------------------------------
 # Subsetting only the sites & species I have full data for right now
 #sites <- unique(establishment$Site)
 #sites
 
 #species <- unique(establishment$Spp)
 #species
-
-
-
 
 # looking at which cores for which we have measurements
 # FOR CHRISTY: what about the cores where we don't have any data?  Will that be added later?
@@ -132,55 +192,7 @@ head(core.deets)
 core.rw <- core.rw[order(row.names(core.rw), decreasing=T),order(names(core.rw))]
 head(core.rw)
 ncol(core.rw)
-
-##########################################################################
-#AGGREGATION STEP
-#taking ring widths and aggregating from the core level to the tree level
-##########################################################################
 # ----------------------------------------------------------------------------
-# aggregate to the tree level using only dated trees where possible
-trees <- unique(substr(names(core.rw), 1, 6)) # listing trees we have measurements for
-tree.rw <- data.frame(array(NA, dim=c(nrow(core.rw), length(trees)))) # a blank data frame to put everything in
-row.names(tree.rw) <- row.names(core.rw)  # labeling the rows with the years from our rwl
-names(tree.rw)<-unique(substr(names(core.rw), 1, 6)) # labeling the columns as trees
-# summary(tree.rw) # this will get really big very quickly
-dim(tree.rw) # 90 trees, 112 years of data
-for(i in unique(trees)){
-  cols <- which(substr(names(core.rw),1,6)==i) # getting the columns we're working with
-  cores <- names(core.rw)[cols] # getting the name of the cores we're working with
-  
-  if(length(cols) == 1){ # if there's only one core, we just take that regardless of wheter it's dated or not
-   	tree.rw[,which(trees==i)] <- core.rw[,cols]
-  	} else { 
-  	# if there's more than 1 core, we need to figure out which if any were dated
-  	use <- vector(length=length(cols))
-	for(x in 1:length(cols)){
-		ifelse(core.data[core.data$CoreID==cores[x], "dated"]=="Y", use[x] <- "TRUE", use[x] <- "FALSE")
-	 }	
-
-	# now we know which were dated, so we can use that to figure out which cores to average
-	if(length(use[use=="TRUE"])==1) { # if only 1 core is dated, use only that core
-  	 	tree.rw[,which(trees==i)] <- core.rw[,cols[which(use=="TRUE")]]
-	} else if(length(use[use=="TRUE"])>1) { # If there's greater than one dated core, take the mean of the dated cores
-		tree.rw[,which(trees==i)] <- rowMeans(core.rw[,cols[which(use=="TRUE")]], na.rm=T)
-	} else { # If no cores are dated, take the mean of whatever we have
-		tree.rw[,which(trees==i)] <- rowMeans(core.rw[,cols], na.rm=T) 
-	}
-  }
-}
-# summary(tree.rw)
-min(tree.rw, na.rm=T)
-dim(tree.rw)
-# ----------------------------------------------------------------------------
-
-# ----------------------------------------------------------------------------
-# ----------------------------------------------------------------------------
-# CRR: writing our tree RWL to a csv and then moving to modeling RWL so we can gap fill before we calculate BAI (bc some have missing outsides and that's bad if we're working outside-in in out calculations)
-
-write.csv(tree.rw, "TreeRWL.csv", row.names=T)
-# ----------------------------------------------------------------------------
-# ----------------------------------------------------------------------------
-
 
 #making dataframe of tree diameter
 tree.deets <- data.frame(names(tree.rw)) 
